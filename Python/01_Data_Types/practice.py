08c925e6dea1f2ab7cae971d19309524bf589d58
anushachandanala2004-ux <anushachandanala2004@gmail.com>
2026-09-19 10:52:19 +0530

    Create practice.py

---
+++ Python/01_Data_Types/practice.py
@@ -0,0 +1,11 @@
+# Python Data Types Practice
+
+# Q1. Create a variable containing your name.
+
+# Q2. Create a variable containing your age.
+
+# Q3. Create a variable containing your salary.
+
+# Q4. Create a Boolean variable.
+
+# Q5. Print the type of each variable.
